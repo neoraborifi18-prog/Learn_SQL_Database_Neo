8a155fe98651e28fef9845b984ac409e6119ac86
neo <neoraborifi18@gmail.com>
2026-05-03 21:49:00 +0200

    Add 02_create_table.sql

---
+++ 02_create_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Students
+(
+    StudentID INT PRIMARY KEY IDENTITY(1,1),
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Email VARCHAR(100) UNIQUE,
+    CreatedDate DATETIME DEFAULT GETDATE()
+);
